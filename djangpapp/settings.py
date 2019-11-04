a1cc1c88703a2966c8f0df69ddd0edbe40751c00
nitesh-solanki <er.nitesh11@gmail.com>
2019-11-04 17:34:51 +0530

    email credentials need to update

--- djangpapp/settings.py
+++ djangpapp/settings.py
@@ -130,5 +130,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS = True  
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
-EMAIL_HOST_USER = 'er.nitesh11@gmail.com'  
-EMAIL_HOST_PASSWORD = '********'
+EMAIL_HOST_USER = 'ENTER_EMAIL'  
+EMAIL_HOST_PASSWORD = 'ENTER_PASSWORD'
