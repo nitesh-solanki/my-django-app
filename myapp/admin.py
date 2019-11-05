abb895c56aa6f8b2e958839effa1785a9d4d6ce7
nitesh-solanki <er.nitesh11@gmail.com>
2019-11-05 10:27:07 +0530

    myapp models: import and register to display at admin side

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from myapp.models import Employee
+from myapp.models import Student
+
+admin.site.register(Student)    # Student is registered
+admin.site.register(Employee)   # Employee is registered
